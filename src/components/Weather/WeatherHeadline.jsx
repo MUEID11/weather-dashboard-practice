bdeff4abb0c7121eb045855cc45afad85ce8aae3
MUEID11 <mehedirangpur3@gmail.com>
2025-01-13 15:04:19 +0600

    weather headline component done

--- src/components/Weather/WeatherHeadline.jsx
+++ src/components/Weather/WeatherHeadline.jsx
@@ -1,23 +1,60 @@
-
 import { useContext } from "react";
-import pinIcon from "../../assets/pin.svg"
+import CloudIcon from "../../assets/cloud.svg";
+import HazeIcon from "../../assets/haze.svg";
+import SnowIcon from "../../assets/icons/snow.svg";
+import SunnyIcon from "../../assets/icons/sunny.svg";
+import RainIcon from "../../assets/rainy.svg";
+import pinIcon from "../../assets/pin.svg";
+import ThunderIcon from "../../assets/thunder.svg"
 import { WeatherContext } from "../../context";
+import { getFormattedDate } from "../../Utils/getFormatedDate";
 const WeatherHeadline = () => {
-  const { weatherData} = useContext(WeatherContext);
+  const { weatherData } = useContext(WeatherContext);
+
+  const { climate, location, temperature, time } = weatherData;
+
+  function getClimateIcon(climate) {
+    switch(climate){
+      case "Rain":
+        return RainIcon;
+      case "Clouds":
+        return CloudIcon;
+      case "Haze":
+        return HazeIcon;
+      case "Clear":
+        return SunnyIcon;
+      case "Snow": 
+        return SnowIcon;
+      case "Thunder":
+        return ThunderIcon;
+      case 'Fog':
+        return HazeIcon;
+      case 'Mist':
+        return HazeIcon;
+
+      default:
+        return SunnyIcon;
+    }
+  }
   return (
     <div>
-          <div className="max-md:flex items-center justify-between md:-mt-10">
-            <img src={'dynamicIcon'} alt="cloud" />
-            <div className="max-md:flex items-center max-md:space-x-4">
-              <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">{weatherData?.temperature}°</h1>
-              <div className="flex items-center space-x-4 md:mb-4">
-                <img src={pinIcon} />
-                <h2 className="text-2xl lg:text-[50px]">Dhaka</h2>
-              </div>
-            </div>
+      <div className="max-md:flex items-center justify-between md:-mt-10">
+        <img src={getClimateIcon(climate)} alt="cloud" />
+        <div className="max-md:flex items-center max-md:space-x-4">
+          <h1 className="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
+            {Math.round(temperature)}°
+          </h1>
+          <div className="flex items-center space-x-4 md:mb-4">
+            <img src={pinIcon} />
+            <h2 className="text-2xl lg:text-[50px]">{location}</h2>
           </div>
-          <p className="text-sm lg:text-lg">06:09 - Sunday, 9 Dec ‘23</p>
         </div>
+      </div>
+      <p className="text-sm lg:text-lg">
+        {getFormattedDate(time, "time", false)} -{" "}
+        {getFormattedDate(time, "date", false)}
+      </p>
+    </div>
   );
 };
 
